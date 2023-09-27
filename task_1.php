6ddd4f1a5157dfa2560736300d7fcc79b2d977e2
Tarek Aziz <tarekcse.lus@gmail.com>
2023-09-27 21:54:20 +0600

    task1 done

---
+++ task_1.php
@@ -0,0 +1,17 @@
+<?php
+function modifyText($text) {
+
+    $text = strtolower($text);
+    
+
+    $text = str_replace("brown", "red", $text);
+    
+    
+    echo $text;
+}
+
+
+$text = "The quick brown fox jumps over the lazy dog.";
+
+modifyText($text);
+?>
